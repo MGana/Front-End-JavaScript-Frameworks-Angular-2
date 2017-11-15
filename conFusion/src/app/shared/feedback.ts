5b099765200552c974f470e20d71d10cb69f3bad
MGana <mohamed.gana@gmail.com>
2017-11-15 20:32:56 +0100

    Angular Reactive Forms Part 1
    * Design a reactive form in your Angular application.
    The structure of the created Feedback class represents the data model, corresponding to the form model that we're going to use within our Angular application.
    ng serve --open : to run the application

---
+++ conFusion/src/app/shared/feedback.ts
@@ -0,0 +1,13 @@
+export class Feedback {
+    firstname: string;
+    lastname: string;
+    telnum: number;
+    email: string;
+    agree: boolean;
+    contacttype: string;
+    message: string;
+};
+
+export const ContactType = ['None', 'Tel', 'Email'];
+
+//this is the structure of the class that represents the data model, corresponding to the form model that we're going to use within our Angular application.

faafd82b1192b38041dc0f45f7258ddc99d0b1f7
MGana <mohamed.gana@gmail.com>
2017-11-16 12:58:43 +0100

    Angular and Promise Part 1
    A promise is a mechanism that supports asynchronous computation.
    In using a promise, the promise provides a mechanism or a proxy for a value that is not available at the moment that you ask for it.
    So a promise will return a promise to you, saying that when the result becomes available, it'll come back and deliver the results to you.
    So a promise returns a proxy object to you, and you hold on to the proxy object.
    
    It may be also immediately available, in which case the promise will return the value immediately.
    But in case it needs to return a value at a future date, then it gives you the promise that that value when it becomes available will be returned to you.
    
    * Our first step in this exercise is to reconfigure all the services to return promises. So each of the metrics within our service, instead of returning the value immediately, it'll return promises.
    * Then, Consume the promises within your components and handle the resolution or rejection of the promise

--- conFusion/src/app/services/dish.service.ts
+++ conFusion/src/app/services/dish.service.ts
@@ -7,15 +7,15 @@ export class DishService {
 
   constructor() { }
 
-  getDishes(): Dish[] {
-    return DISHES;
+//Now, since we have the result already available to us, I'm going to use the shortcut method of returning the promise by resolving the promise immediately. Now, obviously this works well if you have the results immediately available to you, but when you reconfigure your service to be able to go and fetch this data from a server, then we will have to rewrite this code to deal with the fact that the service will not return the result immediately.
+//In the next exercise, we're going to simulate the delay by using a Javascript method, so that it gives you a feel of what it means to deal with delays in receiving a result from a promise. 
+  getDishes(): Promise<Dish[]> {
+    return Promise.resolve(DISHES);
   }
-
-  getDish(id: number): Dish {
-    return DISHES.filter((dish) => (dish.id === id))[0];
+  getDish(id: number): Promise<Dish> {
+    return Promise.resolve(DISHES.filter((dish) => (dish.id === id))[0]);
   }
-
-  getFeaturedDish(): Dish {
-    return DISHES.filter((dish) => dish.featured)[0];
+  getFeaturedDish(): Promise<Dish> {
+    return Promise.resolve(DISHES.filter((dish) => dish.featured)[0]);
   }
 }

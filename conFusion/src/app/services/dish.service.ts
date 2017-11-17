657191a5c56e83189490830c1ab0317c61a8402b
MGana <mohamed.gana@gmail.com>
2017-11-17 14:16:29 +0100

    Angular and RxJS Part 1
    * Make use of observables within your Angular application (convert the services to use Observables in place of promises)
    * Obtain data through subscribing to the observables within your components.
    
    Reactive programming is based around observer pattern and also an iterated pattern.
    in reactive programming, we are interested in data flows. How data flows through your application.
    Any data flow can be treated as a stream. So this data flow can propagate changes throughout your application.
    (at intermediary stages you can even take one particular stream, modify it and create another stream going out from there. So that is what an observable pattern enables us to drew.)
    
    RxJS is a JavaScript implementation of the observable pattern and also support for reactive programming.
    RxJS is a library that enables you to do asynchronous programming.
    So, when you are wanting to wait for an asynchronous event, it will be similar to subscribing to an observable.
    So when that observable emits the value, you are ready to catch the value and then act accordingly. You do know when that emission is going to take place.
    So that is the asynchronous part coming into play with the use of RxJS.
    Angular leverages RxJS for its support for reactive programming.
    Forms themselves become observables in Angular. And any changes in the values of any of the form elements, can be outputted, and observed by subscribing to the observable that the Angular framework provides for us.

--- conFusion/src/app/services/dish.service.ts
+++ conFusion/src/app/services/dish.service.ts
@@ -2,27 +2,27 @@ import { Injectable } from '@angular/core';
 import { Dish } from '../shared/dish';
 import { DISHES } from '../shared/dishes';
 
+import { Observable } from 'rxjs/Observable';
+//import 'rxjs/add/operator/toPromise';
+import 'rxjs/add/operator/delay';
+import 'rxjs/add/observable/of';
+
+
 @Injectable()
 export class DishService {
 
   constructor() { }
 
-  getDishes(): Promise<Dish[]> {
-    return new Promise(resolve=> {
-      // Simulate server latency with 2 second delay
-        setTimeout(() => resolve(DISHES), 2000);
-    });
+  getDishes(): Observable<Dish[]> {
+    //return Observable.of(DISHES).delay(2000).toPromise();
+    return Observable.of(DISHES).delay(2000);  
   }
-  getDish(id: number): Promise<Dish> {
-    return new Promise(resolve=> {
-      // Simulate server latency with 2 second delay
-        setTimeout(() => resolve(DISHES.filter((dish) => (dish.id === id))[0]), 2000);
-    });
+  getDish(id: number): Observable<Dish> {
+    return Observable.of(DISHES.filter((dish) => (dish.id === id))[0]).delay(2000);
   }
-  getFeaturedDish(): Promise<Dish> {
-    return  new Promise(resolve=> {
-      // Simulate server latency with 2 second delay
-        setTimeout(() => resolve(DISHES.filter((dish) => dish.featured)[0]), 2000);
-    });
+  getFeaturedDish(): Observable<Dish> {
+    return Observable.of(DISHES.filter((dish) => dish.featured)[0]).delay(2000);
   }
 }
+
+//We will now let our services return Observables, and we will update the components to make use of Observables. The reason, as I mentioned, is because when we update our services to use the HTTP service to go to a server to fetch the data, then we would note that in Angular, the HTTP methods will return Observables. And, the services can simply pass the Observables on to our components and let the components subscribe to these Observables and make use of the data that they obtained thereby. 

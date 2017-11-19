34dbbe436e5e85ed222180301c2ef191d3f20308
MGana <mohamed.gana@gmail.com>
2017-11-19 18:47:46 +0100

    Angular and RxJS Part 2
    * Learn to use the built-in observables within Angular
    * Leverage the built in observables to implement new features within your Angular application

--- conFusion/src/app/dishdetail/dishdetail.component.ts
+++ conFusion/src/app/dishdetail/dishdetail.component.ts
@@ -7,6 +7,8 @@ import { Location } from '@angular/common';
 import { Dish } from '../shared/dish';
 import { DishService } from '../services/dish.service';
 
+import 'rxjs/add/operator/switchMap';
+
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
@@ -15,15 +17,36 @@ import { DishService } from '../services/dish.service';
 export class DishdetailComponent implements OnInit {
 
   dish: Dish;
+  dishIds: number[];
+  prev: number;
+  next: number;
 
   constructor(private dishservice: DishService,
     private route: ActivatedRoute,
     private location: Location) { }
 
-  ngOnInit() {
+  /*ngOnInit() {
     let id = +this.route.snapshot.params['id'];
     this.dishservice.getDish(id).subscribe(dish => this.dish = dish);
+  }*/
+    
+  // what happens is that whenever the parameter, params observable changes value, which means that the route parameter changes value. Then immediately the switchMap operator will take the params value and then do a getDish from my dish server. So this will be automatically initiated. And this will be available as the other observable that is emitted by doing the switchMap operator on this observable.     
+  ngOnInit() {
+    this.dishservice.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
+    this.route.params
+      .switchMap((params: Params) => this.dishservice.getDish(+params['id']))
+      .subscribe(dish => { this.dish = dish; this.setPrevNext(dish.id); });
   }
+  //what happens is every time my dish gets updated, I will update the previous and the next also, correspondingly, from here
+    
+  setPrevNext(dishId: number) {
+    let index = this.dishIds.indexOf(dishId);
+    this.prev = this.dishIds[(this.dishIds.length + index - 1)%this.dishIds.length];
+    this.next = this.dishIds[(this.dishIds.length + index + 1)%this.dishIds.length];
+  }
+  //to be able to navigate through my list of dishes, I need to know the set of all my dishes. Or at least, I need to know the ID's of all my dishes. And that is the reason why I use this dishIds here. 
+  //Now, if index is 1, index minus 1 will give me zero. If index is zero, index minus 1 will give me minus 1, but that's not what I want. I want to wrap around, and get dishId length minus 1. So that's why I am including this in there, and then doing a modulo with the dishIds. So when I reach, when index is zero, I will wrap around to get the last item in my array.    
+
 
   goBack(): void {
     this.location.back();
--- conFusion/src/app/services/dish.service.ts
+++ conFusion/src/app/services/dish.service.ts
@@ -23,6 +23,9 @@ export class DishService {
   getFeaturedDish(): Observable<Dish> {
     return Observable.of(DISHES.filter((dish) => dish.featured)[0]).delay(2000);
   }
+  getDishIds(): Observable<number[]> {
+    return Observable.of(DISHES.map(dish => dish.id ));
+  } 
 }
 
 //We will now let our services return Observables, and we will update the components to make use of Observables. The reason, as I mentioned, is because when we update our services to use the HTTP service to go to a server to fetch the data, then we would note that in Angular, the HTTP methods will return Observables. And, the services can simply pass the Observables on to our components and let the components subscribe to these Observables and make use of the data that they obtained thereby. 

31b535f48a735a4da95b4e34d166bbd6696b91ee
MGana <mohamed.gana@gmail.com>
2017-11-20 21:05:00 +0100

    Assignment 3: Angular Forms and Reactive Programming

--- conFusion/src/app/dishdetail/dishdetail.component.ts
+++ conFusion/src/app/dishdetail/dishdetail.component.ts
@@ -9,6 +9,9 @@ import { DishService } from '../services/dish.service';
 
 import 'rxjs/add/operator/switchMap';
 
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Comment } from '../shared/comment';
+
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
@@ -20,36 +23,91 @@ export class DishdetailComponent implements OnInit {
   dishIds: number[];
   prev: number;
   next: number;
+    
+  commentForm: FormGroup;
+  newcomment: Comment;
+  formErrors = {
+    'author': '',
+    'comment': ''
+  };
+  validationMessages = {
+    'author': {
+      'required':      'Name is required.',
+      'minlength':     'Name must be at least 2 characters long.'
+    },
+    'comment': {
+      'required':      'Your comment is required.'
+    }
+  };
 
   constructor(private dishservice: DishService,
     private route: ActivatedRoute,
-    private location: Location) { }
+    private location: Location,
+    private fb: FormBuilder) {
+        this.createCommentForm();
+  }
 
-  /*ngOnInit() {
-    let id = +this.route.snapshot.params['id'];
-    this.dishservice.getDish(id).subscribe(dish => this.dish = dish);
-  }*/
-    
-  // what happens is that whenever the parameter, params observable changes value, which means that the route parameter changes value. Then immediately the switchMap operator will take the params value and then do a getDish from my dish server. So this will be automatically initiated. And this will be available as the other observable that is emitted by doing the switchMap operator on this observable.     
   ngOnInit() {
     this.dishservice.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
     this.route.params
       .switchMap((params: Params) => this.dishservice.getDish(+params['id']))
       .subscribe(dish => { this.dish = dish; this.setPrevNext(dish.id); });
   }
-  //what happens is every time my dish gets updated, I will update the previous and the next also, correspondingly, from here
-    
+
+
   setPrevNext(dishId: number) {
     let index = this.dishIds.indexOf(dishId);
     this.prev = this.dishIds[(this.dishIds.length + index - 1)%this.dishIds.length];
     this.next = this.dishIds[(this.dishIds.length + index + 1)%this.dishIds.length];
   }
-  //to be able to navigate through my list of dishes, I need to know the set of all my dishes. Or at least, I need to know the ID's of all my dishes. And that is the reason why I use this dishIds here. 
-  //Now, if index is 1, index minus 1 will give me zero. If index is zero, index minus 1 will give me minus 1, but that's not what I want. I want to wrap around, and get dishId length minus 1. So that's why I am including this in there, and then doing a modulo with the dishIds. So when I reach, when index is zero, I will wrap around to get the last item in my array.    
 
 
   goBack(): void {
     this.location.back();
   }
 
+  createCommentForm() {
+    this.commentForm = this.fb.group({
+      author: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)] ],
+      rating: 5,
+      comment: ['', [Validators.required] ],
+    });
+
+    this.commentForm.valueChanges
+      .subscribe(data => this.onValueChanged(data));
+
+    this.onValueChanged(); // (re)set validation messages now
+
+  }
+
+  onValueChanged(data?: any) {
+    if (!this.commentForm) { return; }
+    this.newcomment = this.commentForm.value;             
+    const form = this.commentForm;
+    for (const field in this.formErrors) {
+      // clear previous error message (if any)
+      this.formErrors[field] = '';
+      const control = form.get(field);
+      if (control && control.dirty && !control.valid) {
+        const messages = this.validationMessages[field];
+        for (const key in control.errors) {
+          this.formErrors[field] += messages[key] + ' ';
+        }
+      }
+    }
+  }
+
+  onSubmit() {
+    this.newcomment = this.commentForm.value;
+    this.newcomment.date = new Date().toISOString();             
+    console.log(this.newcomment);
+    this.dish.comments.push(this.newcomment);
+    this.commentForm.reset({
+      author: '',
+      rating: 5,
+      comment: ''
+    });
+  }
+
+    
 }

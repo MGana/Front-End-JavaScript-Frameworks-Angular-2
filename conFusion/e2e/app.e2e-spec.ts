76a4fa11b6fac68708b012162793600ecc1e0a79
MGana <mohamed.gana@gmail.com>
2017-11-28 13:28:34 +0100

    End-to-End Test: Test2

--- conFusion/e2e/app.e2e-spec.ts
+++ conFusion/e2e/app.e2e-spec.ts
@@ -11,4 +11,14 @@ describe('con-fusion App', () => {
     page.navigateTo('/');
     expect(page.getParagraphText('app-root h1')).toEqual('Ristorante Con Fusion');
   });
+    
+  it('should navigate to about us page by clicking on the link', () => {
+    page.navigateTo('/');
+
+    let navlink = page.getAllElements('a').get(1);
+    navlink.click();
+
+    expect(page.getParagraphText('h3')).toBe('About Us')
+  });
+    
 });

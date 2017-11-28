db3c0f8832b281edbc5c3011d4e8a0f39dd52ecd
MGana <mohamed.gana@gmail.com>
2017-11-28 13:31:08 +0100

    End-to-End Test: Test3
    another test to app.e2e-spec.ts to enable submission of a form

--- conFusion/e2e/app.e2e-spec.ts
+++ conFusion/e2e/app.e2e-spec.ts
@@ -1,4 +1,5 @@
 import { AppPage } from './app.po';
+import { browser } from 'protractor';
 
 describe('con-fusion App', () => {
   let page: AppPage;
@@ -19,6 +20,21 @@ describe('con-fusion App', () => {
     navlink.click();
 
     expect(page.getParagraphText('h3')).toBe('About Us')
+  });
+    
+  it('should enter a new comment for the first dish', () => {
+    page.navigateTo('/dishdetail/0');
+
+    let newAuthor = page.getElement('input[type=text]');
+    newAuthor.sendKeys('Test Author');
+
+    let newComment = page.getElement('textarea');
+    newComment.sendKeys('Test Comment');
+
+    let newSubmitButton = page.getElement("button[type=submit]");
+    newSubmitButton.click();
+
+    browser.pause();
   });
     
 });
